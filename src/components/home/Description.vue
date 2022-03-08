ad13e8f9935cb06a72dd81269ff4bb592e268c2f
jose piguz <josepiguz@gmail.com>
2022-03-08 13:56:04 -0400

    listo

--- src/components/home/Description.vue
+++ src/components/home/Description.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="row flex justify-center items-center q-pa-xl">
-    <div class=" col-12 text-h4 text-justify text-bold">
+  <div class="row flex justify-center items-center q-pa-xl q-gutter-xl">
+    <div class=" col-12 text-h4 text-justify text-bold ">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
     </div>
-    <div class=" col-12  text-justify text-bold">
+    <div class=" col-12  text-justify ">
        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
 
        
     </div>
-    <div class=" col-12">
+    <div class=" col-12 ">
       <q-btn color="primary" label="hacer cotizacion" class="full-width"  @click="$router.push('/budget')" />
     </div>
   </div>
