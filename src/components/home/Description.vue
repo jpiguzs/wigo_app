46a3ed6b26a5adde81b172057378f566d0093dd7
jose piguz <josepiguz@gmail.com>
2022-03-09 13:35:03 -0400

    cambio de titulos

--- src/components/home/Description.vue
+++ src/components/home/Description.vue
@@ -9,8 +9,8 @@
     />
       </q-avatar>
     </div>
-    <div class=" col-12  text-justify ">
-       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    <div class=" col-12 text-h6 text-center ">
+      Siempre a Tiempo con tus entregas
 
        
     </div>
